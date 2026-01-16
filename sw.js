199beea3a9fa1c2259ffe0a7400c214403d88d82
billy4select <billy4select@gmail.com>
2026-01-16 09:52:35 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'finance-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json'
+];
+
+// 安裝並快取檔案
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// 攔截請求，實現離線使用
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
